002282b6b23452b74e87ca8f090bb2b785062c55
Akihiko Nakamura <anakamuro@earthlink.net>
2023-10-30 12:13:03 -0400

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+const apiKey = "tlOgIfcg9USeK86M6Ckanw==ApXJsjqEHeAopuSq";
+
+const options = {
+    method: "GET", 
+    headers: {
+        "X-Api-Key": apiKey,
+    }
+}
+
+const apiURL = "https://api.api-ninjas.com/v1/jokes?limit=1"
+
+async function getJoke(){
+
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+    
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+    
+        jokeEl.innerHTML = data[0].joke;
+        console.log(data); 
+    } catch (error) {
+     jokeEl.innerText = "An error happened, try again later";
+     btnEl.disabled = false;
+     btnEl.innerText = "Tell me a joke";
+     console.log(error);
+    }
+}
+
+
+btnEl.addEventListener("click", getJoke);
